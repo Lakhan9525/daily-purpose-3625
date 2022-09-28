ee9f8334c465ae16df1932ee0451cb7d21801cad
Omgupta12 <rahulgupta1997242@gmail.com>
2022-09-28 22:24:22 +0530

    footer

--- Frontend/src/Pages/AllRoutes.jsx
+++ Frontend/src/Pages/AllRoutes.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
+import Footer from '../Components/Footer/Footer';
 import FinalFeature from './Feature/FinalFeature'
 
 
 const AllRoutes = () => {
   return (
     <>
-    <Routes>
-    <Route path="/feature" element={<FinalFeature />}></Route>
-    </Routes>
+      <Routes>
+
+        <Route path="/feature" element={<FinalFeature />}></Route>
+      </Routes>
+
+      <Footer />
 
     </>
   )
