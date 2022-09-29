fe8d3c7c7e3c870152568e4fdb6252a49e6d977b
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-09-30 00:30:08 +0530

    signup day-3

--- Frontend/src/Pages/AllRoutes.jsx
+++ Frontend/src/Pages/AllRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Routes } from "react-router-dom";
 import FinalFeature from './Feature/FinalFeature'
+import Signup from './Signup/Signup';
 
 
 const AllRoutes = () => {
   return (
     <>
       <Routes>
-
+        <Route path='/signup' element={<Signup/>}/>
         <Route path="/feature" element={<FinalFeature />}></Route>
       </Routes>
 
