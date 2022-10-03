8b6da4be36fb2f90f6bb474a53819db27829fdeb
ShirsoBhattacharyya <101390681+ShirsoBhattacharyya@users.noreply.github.com>
2022-10-03 15:04:38 +0530

    undo-backend-deployment

--- Backend/index.js
+++ Backend/index.js
@@ -27,8 +27,8 @@ app.use(
   })
 );
 app.use("/auth", authRoute);
-const PORT=process.env.PORT || 8080;
-app.listen(PORT, () => {
+
+app.listen('8080', () => {
   mongoose.connect(
     "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.5.4"
   );
